567600c42e9ce4b2a61f81501a330a2e475f28bd
elporavv <andyvercha@gmail.com>
2025-09-23 21:35:21 -0300

    Version con suscripcion

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { HelpComponent } from './components/help/help.component';
 import { InvoiceExplorerComponent } from './components/invoice-explorer/invoice-explorer.component';
 import { InvoicesV2Component } from './components/invoices-v2/invoices-v2.component';
 import { LoginComponent } from './components/login/login.component';
+import { SubscriptionComponent } from './components/subscription/subscription.component';
 import { ExportTemplatesComponent } from './components/export-templates/export-templates.component';
 import { TemplateEditorComponent } from './components/export-templates/template-editor.component';
 import { TemplateExportComponent } from './components/export-templates/template-export.component';
@@ -28,6 +29,7 @@ const routes: Routes = [
   { path: 'templates-export/edit/:id', component: TemplateEditorComponent, canActivate: [AuthGuard] },
   { path: 'templates-export/export/:id', component: TemplateExportComponent, canActivate: [AuthGuard] },
   { path: 'templates-export/export', component: TemplateExportComponent, canActivate: [AuthGuard] },
+  { path: 'subscription', component: SubscriptionComponent, canActivate: [AuthGuard] },
   { path: 'ayuda', component: HelpComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
